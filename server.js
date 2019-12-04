28419ba5f1eb61ab1085276211c940c9336c23c3
Todd Adcox <TAdcox009@west-mec.org>
2019-12-03 18:28:58 -0700

    .

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const hbs = require('express-handlebars');
+
+app.use(express.static('public'))
+
+app.engine('handlebars', hbs());
+app.set('view engine', 'handlebars');
+
+// Routing
+app.get('/',  (req, res) => {
+    res.render('index', { layout: false });
+});
+
+app.get('/education',  (req, res) => {
+    res.render('education', { layout: false });
+});
+
+app.get('/skillset',  (req, res) => {
+    res.render('skillset', { layout: false });
+});
+
+app.get('/workexp',  (req, res) => {
+    res.render('workexp', { layout: false });
+});
+
+app.listen(8080, () => {
+    console.log('Server is starting at port ', 8080);
+});
